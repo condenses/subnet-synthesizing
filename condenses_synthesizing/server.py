59b23fb370a82a5730955786e38c2e327dd5f138
toilaluan <tranthanhluan.nd@gmail.com>
2025-02-21 05:54:52 +0000

    update

--- condenses_synthesizing/server.py
+++ condenses_synthesizing/server.py
@@ -7,8 +7,10 @@ from .config import CONFIG
 NUM_PROC = 16
 app = FastAPI()
 
+
 class SynthesizingResponse(BaseModel):
     user_message: str
+
 
 # Load dataset once at module level
 instruct_dataset = load_dataset(
@@ -24,6 +26,9 @@ instruct_dataset = instruct_dataset.filter(
     lambda x: len(x["text"]) // 4 > CONFIG.synthesizing.min_tokens
     and len(x["text"]) // 4 < CONFIG.synthesizing.max_tokens,
 )
+
+instruct_dataset = iter(instruct_dataset)
+
 
 @app.get("/api/synthesizing", response_model=SynthesizingResponse)
 def api_synthesizing() -> SynthesizingResponse:
